5381532926f2685c85cda63ed180f735f9fd5780
Alex Suraci <suraci.alex@gmail.com>
2022-06-18 09:37:55 -0400

    repro 137

--- 137/controller/controller.go
+++ 137/controller/controller.go
@@ -3,23 +3,18 @@ package controller
 import (
 	context "context"
 
-	"github.com/livebud/issues/137/db"
-	model "github.com/livebud/issues/137/model/post"
+	"github.com/livebud/issues/137/model/post"
 )
 
 type Controller struct {
 }
 
-// Index of posts
-// GET
-func (c *Controller) Index(ctx context.Context) (posts []*db.Post, err error) {
-	model := &model.Model{}
-	return model.FindMany()
+type Post struct {
+	Model post.Model
 }
 
 // Show post
 // GET /:id
-func (c *Controller) Show(ctx context.Context, id int) (post *db.Post, err error) {
-	model := &model.Model{}
-	return model.Find(id)
+func (c *Controller) Show(ctx context.Context, id int) (post *Post, err error) {
+	return &Post{}, nil
 }
